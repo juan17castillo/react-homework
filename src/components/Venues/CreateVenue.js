e89d566dbe5ce11a3a194de53abb7f00b5c2c41c
Juan Manuel Castillo <juancastillo9910@gmail.com>
2021-04-18 13:11:29 -0500

    fix validations

--- src/components/Venues/CreateVenue.js
+++ src/components/Venues/CreateVenue.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Navbar from "../Navbar";
 import fire from "../../config/firebase";
 import SimpleReactValidator from "simple-react-validator";
-import es from '../../config/es'
+import es from "../../config/es";
 class CreateVenue extends Component {
   constructor() {
     super();
-    SimpleReactValidator.addLocale('es', es)
+    SimpleReactValidator.addLocale("es", es);
     this.validator = new SimpleReactValidator({ locale: "es" });
   }
 
@@ -43,8 +44,6 @@ class CreateVenue extends Component {
       this.state.db.collection("venues").doc().set(this.state.venue);
     } else {
       this.validator.showMessages();
-      // rerender to show messages for the first time
-      // you can use the autoForceUpdate option to do this automatically`
       this.forceUpdate();
     }
   };
@@ -52,88 +51,111 @@ class CreateVenue extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <div className="container text-center">
           <h1 className="my-5">Crear sede</h1>
           <div className="card col-md-12 py-5 my-5">
             <form onSubmit={this.saveVenue}>
               <div class="form-row col-md-12">
                 <div class="form-group col-md-6">
-                  <label for="inputText">Nombre</label>
+                  <label htmlFor="name">Nombre</label>
                   <input
                     type="text"
                     class="form-control"
                     name="name"
                     ref={this.nameRef}
                     onChange={this.changeState}
-                    id="inputText"
                     placeholder="Jhon Doe"
                   />
-                  {this.validator.message("nombre", this.state.name, "required|alpha_num")}
+                  {this.validator.message(
+                    "nombre",
+                    this.state.venue.name,
+                    "required|alpha"
+                  )}
                 </div>
                 <div class="form-group col-md-6">
-                  <label for="inputTel">Teléfono</label>
+                  <label htmlFor="phone">Teléfono</label>
                   <input
                     type="tel"
                     class="form-control"
                     name="phone"
                     ref={this.phoneRef}
                     onChange={this.changeState}
-                    id="inputTel"
                     placeholder="(+57) 555 555 55 55"
                   />
-                   {this.validator.message("teléfono", this.state.phone, "required|numeric")}
+                  {this.validator.message(
+                    "teléfono",
+                    this.state.venue.phone,
+                    "required|phone"
+                  )}
                 </div>
               </div>
               <div class="form-row col-md-12">
                 <div class="form-group col-md-4">
-                  <label for="inputEmail4">Email</label>
+                  <label htmlFor="email">Email</label>
                   <input
                     type="email"
                     class="form-control"
                     name="email"
                     ref={this.emailRef}
                     onChange={this.changeState}
-                    id="inputEmail4"
                     placeholder="Email"
                   />
+                  {this.validator.message(
+                    "correo",
+                    this.state.venue.email,
+                    "required|email"
+                  )}
                 </div>
                 <div class="form-group col-md-8">
-                  <label for="inputAddress2">Dirección</label>
+                  <label htmlFor="address">Dirección</label>
                   <input
                     type="text"
                     class="form-control"
                     name="address"
                     ref={this.addressRef}
                     onChange={this.changeState}
-                    id="inputAddress2"
                     placeholder="Avenida 555 calle 55"
                   />
+                  {this.validator.message(
+                    "dirección",
+                    this.state.venue.address,
+                    "required|alpha_num"
+                  )}
                 </div>
               </div>
               <div class="form-row col-md-12">
                 <div class="form-group col-md-8">
-                  <label for="inputText">Ciudad</label>
+                  <label htmlFor="city">Ciudad</label>
                   <input
                     type="text"
                     class="form-control"
                     ref={this.cityRef}
                     onChange={this.changeState}
                     name="city"
-                    id="inputText"
                     placeholder="Cali"
                   />
+                  {this.validator.message(
+                    "ciudad",
+                    this.state.venue.city,
+                    "required|alpha"
+                  )}
                 </div>
                 <div class="form-group col-md-4">
-                  <label for="inputText">Código Zip</label>
+                  <label htmlFor="zipCode">Código Zip</label>
                   <input
                     type="text"
                     class="form-control"
                     name="zipCode"
                     ref={this.zipCodeRef}
                     onChange={this.changeState}
-                    id="inputText"
                     placeholder="760026"
                   />
+                  {this.validator.message(
+                    "código zip",
+                    this.state.venue.zipCode,
+                    "required|alpha"
+                  )}
                 </div>
               </div>
               <div class="form-group">
@@ -144,9 +166,8 @@ class CreateVenue extends Component {
                     ref={this.activeRef}
                     onChange={this.changeState}
                     type="checkbox"
-                    id="gridCheck"
                   />
-                  <label class="form-check-label" for="gridCheck">
+                  <label class="form-check-label" htmlFor="active">
                     Activa
                   </label>
                 </div>
